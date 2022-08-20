66cf1c5367362ff56b3c284ef21f2c07f30bd26e
ivanutsmdsi <69378200+ivanutsmdsi@users.noreply.github.com>
2022-08-20 23:14:25 +1000

    removed test code from file and added comments to clarify code processing

--- scholarly_web_scrapper.py
+++ scholarly_web_scrapper.py
@@ -7,25 +7,21 @@
 import pandas as pd
 from scholarly import scholarly
 
+#execute web scrap
 search_query = scholarly.search_pubs(query = 'construction health safety')
 
-#data = next(search_query)
-#df = pd.json_normalize(data)
-#
-#print('df.T running')
-#df.T
-#
-#df.to_csv('scholarly_data.csv')
-
+# store first result into dataframe to build the dataframe
 data = next(search_query)
 df = pd.json_normalize(data)
-i = 1
+
+# iterate through the rest of the web scrap results
+i = 1  
 for data in search_query:
     print(i)
     if i == 20:
-            break
+            break                   # putting a hard limit so I don't exceed Google HTTP requests
     temp = pd.json_normalize(data)
-    df = pd.concat([df, temp])
+    df = pd.concat([df, temp])      # append each new result into the dataframe for export later
     i = i + 1
 
 df.to_csv('scholarly_data.csv')
