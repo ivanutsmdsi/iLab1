d8e8e1e91c49726127d43be9d8a83e1b072801dc
ivanutsmdsi <69378200+ivanutsmdsi@users.noreply.github.com>
2022-09-14 23:25:37 +1000

    Rewrote the PLOS API extractor with the same main() method and arguments as news_scrapper_2.py

--- plos_scrapper.py
+++ plos_scrapper.py
@@ -1,120 +1,138 @@
+import requests
 import argparse
-import feedparser
 import pandas as pd
 import sys
+import math
 import time
 from datetime import datetime
 from datetime import date
 import dateutil.relativedelta
 
 ## init a blank pandas df shell to append results to
-news_df = pd.DataFrame(columns=['Title','Source','Published'])
+abs_df = pd.DataFrame(columns=['journal','published','title', 'abstract'])
 
 ##
 # save dataframe to output file
 ##
 def save_to_csv():
-    global news_df
-    save_csv = open("gnews_output.csv","w",newline='',encoding='utf-8')
-    news_df.to_csv('gnews_output.csv')
+    global abs_df
+    save_csv = open("plos_output.csv","w",newline='',encoding='utf-8')
+    abs_df.to_csv('plos_output.csv')
     save_csv.close()
-    print("Google news search results saved to 'gnews_output.csv'")
+    print("PLOS search results saved to 'plos_output.csv'")
     return
 
 ##
 # returns the pandaframe
 ##
 def get_df():
-    global news_df
-    return news_df
+    global abs_df
+    return abs_df
 
 ##
-# Save the entries in a feed object to the main news_df dataframe, appending to previous entries
+# Save the entries in a feed object to the main abs_df dataframe, appending to previous entries
 ##
-def parse_feed_to_df(feed):
-    global news_df
+def parse_response_to_df(docs):
+    global abs_df
     
-    # print('num of entries retrieved: ' + str(len(feed['entries']))) 
     
-    for entry in feed['entries']:
-        title = entry['title']
-        source = entry['source']['title']
-        published = entry['published']
-        df2 = pd.DataFrame([[title, source, published]],columns=['Title','Source','Published'])
-        news_df = pd.concat([news_df, df2], ignore_index=True)
+    for article in docs:
+        title = article['title']
+        journal = article['journal']
+        published = article['publication_date']
+        abstract = article['abstract']
+        df2 = pd.DataFrame([[journal, published, title, abstract]],columns=['journal','published','title', 'abstract'])
+        abs_df = pd.concat([abs_df, df2], ignore_index=True)
 
     return
 
 ##
-# Collect response from google news based on search patterns
-# return the feed object
+# Collect response from PLOS search based on search params
+# feed the response through the parser to collect data and save to DF
+# iterate through the response until no more response is available
 ##
 
-def get_feed(query, before, after):
-    params = 'q=' + query + '+before:' + before.strftime('%Y-%m-%d') + '+after:' + after.strftime('%Y-%m-%d')
-    print(params)
-    addr = 'https://news.google.com/rss/search?' + params
-    feed = feedparser.parse(addr)
-    return feed
+def get_plos_results(query, before, after,start_row = 1):
+    # (publication_date:[2022-08-01T00:00:00Z TO 2022-08-31T23:59:59Z]) AND (construction+health+safety)
+    pub_param = '(publication_date:[' + after + 'T00:00:00Z' + ' TO ' + before + 'T00:00:00Z' + '])'
+    join_param = ' AND '
+    query_param = '(' + query + ')' + join_param + pub_param
+
+    fields_param = '&fl=id,title,abstract,publication_date,journal'
+
+    start_param = '&start=' + str(start_row)
+
+    params = 'q=' + query_param + fields_param + start_param
+    
+    # print(params)
+
+    url = 'https://api.plos.org/search?rows=100&' + params
+    response = requests.get(url)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        print('PLOS search request errored out: HTTP code [' + response.status_code + ']')
+        return
 
 ##
-# Analyse the arguments returned and then build a search pattern against Google News RSS feed
+# Analyse the arguments returned and then build a search request to PLOS API
+# Feed in the search request to extract_
 ##
-def get_news(q, lm = False, m = False, d_range = False, before = None, after = None):
-    global news_df
-    print('[Begin Google News article title extraction]')
+def extract_abstracts(q, lm = False, m = False, d_range = False, before = None, after = None):
+    global abs_df
+    print('[Begin PLOS article extraction]')
 
     ## if no search range provided, default to last month
     if (m is False and d_range is False):
         lm = True
 
-    ## Set date range                               ## at least one of these must be true (lm is default option)
+    ## Set date range and replace before and after                     ## at least one of these must be true (lm is default option)
     if(lm):
         range = get_prev_month()
+        before = range['end']
+        after = range['start']
     elif(m):
         range = get_curr_month()
+        before = range['end']
+        after = range['start']
     
-    ## Type A: search once
-    if(d_range is False):
-        print('Requesting search results from google news (1 of 1)..      ', end = '')
-        feed = get_feed(query = q, before = range['end'], after = range['start'])   ## Execute one get request
-        parse_feed_to_df(feed)                                                      ## parse feed into news_df
+    ## Type A: search and iterate through results
+    response = get_plos_results(query = q, before = before, after = after)
 
-    ## Type B: iterate through searches until date is reached
-    elif(d_range is True):
-        r = dateutil.relativedelta.relativedelta(before.replace(day = 1), after.replace(day = 1))
-        months = (r.months + 1) + (r.years * 12)
-        c_month = 1
+    if (response is None):
+        ## end extractions
+        return
+    
+    numFound = response['response']['numFound']
+    currPage = 0
+    numPages = math.ceil(numFound/100)
+
+    print('PLOS article search found ' + str(numFound) + ' articles.')
+
+    results = len(response['response']['docs'])
+    while (currPage < numPages):
+        print('Extracting article search results from PLOS (' + str(currPage + 1) + ' of '+ str(numPages) + ')..      ', end = '')
+
+        ## analyse response first
+        parse_response_to_df(response['response']['docs'])
+
         
-        ## set dates
-        d_before = before
-        d_after = before.replace(day = 1)
+        ## iterate to the next page
+        ## stop if the search reached the end of results
+        currPage = currPage + 1
+        if(currPage * 100 >= numFound):
+            break
 
-        ## if start date is earlier than requested. Set start date to requested date
-        if (d_after < after):
-            d_after = after
+        time.sleep(3)
+        response = get_plos_results(query = q, before = before, after = after, start_row = currPage * 100)
 
-        ## search by months
-        while(d_before >= after):
-            print("Requesting search results from google news (" + str(c_month) + " of " + str(months) + ")...      ", end = '')
-            get_news_by_month(d_before, d_after, q)
+         ## end extractions if nothing was returned
+        if (response is None):
+            return
 
-            ##go to the previous month
-            prev_month = get_prev_month(d_after)
-            d_before = d_after - dateutil.relativedelta.relativedelta(days=1)
-            d_after = datetime.strptime(prev_month['start'], "%Y-%m-%d %H:%M:%S")
-            ## if start date is earlier than requested. Set start date to requested date
-            if (d_after < after):
-                d_after = after
-            ## change the month count
-            c_month += 1
-            time.sleep(6)
-
+          
     return
-
-def get_news_by_month(b, a, q):
-        feed = get_feed(query = q, before = b, after = a)   ## Execute one get request
-        parse_feed_to_df(feed)                                                      ## parse feed into news_df
 
 ##
 #   Print error statments for invalid argument combinations
@@ -122,7 +140,7 @@ def get_news_by_month(b, a, q):
 def invalid_args(error):
     match error:
         case 1:
-            print("Not enough arguments found, call 'news_scrapper_2.py --help' for guidance.")
+            print("Not enough arguments found, call 'plos_scrapper_2.py --help' for guidance.")
         case 2:
             print("cannot use -lm (last month) or -m (current month) or -before & -after together. Refer to documentation for guidance.")
         case 3:
@@ -213,9 +231,9 @@ def main():
 
     ##
     #   Part 2:
-    #   after checking arguments are valid, pass arguments to search_news to build feed query
+    #   after checking arguments are valid, pass arguments to extract_abstracts to build feed query
     ## 
-    get_news(lm = pa.lm, m = pa.m, d_range = d_range, before = pa.before, after = pa.after, q = pa.query_param)
+    extract_abstracts(lm = pa.lm, m = pa.m, d_range = d_range, before = pa.before, after = pa.after, q = pa.query_param)
 
     ##
     #   Part 3:
